ced1dc1df567f2c91c4a9f0ba99c7047393d625c
Naveen Karduri <nkarduri@cfri.ca>
2015-04-22 16:45:25 -0700

    API token project ID order by desc

--- sql_list.php
+++ sql_list.php
@@ -142,7 +142,7 @@ class MySQL {
      "SELECT userrights.project_id, userinfo.username,userinfo.user_firstname, userinfo.user_lastname, userinfo.user_email
      FROM redcap_user_information userinfo
      INNER JOIN redcap_user_rights userrights ON userinfo.username = userrights.username
-     AND userrights.api_token !=  'NULL'";  
+     AND userrights.api_token !=  'NULL' ORDER BY userrights.project_id DESC";  
     }
     
     
